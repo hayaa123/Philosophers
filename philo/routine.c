f18c311c81cb6aa29c4482d9c3cd26ff2584d777
hayaa123 <hayalawansah@gmail.com>
2026-01-27 13:45:21 +0300

    handled one philo edge case

--- philo/routine.c
+++ philo/routine.c
@@ -6,11 +6,27 @@
 /*   By: hal-lawa <hal-lawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 12:10:00 by haya              #+#    #+#             */
-/*   Updated: 2026/01/24 12:13:28 by hal-lawa         ###   ########.fr       */
+/*   Updated: 2026/01/27 13:43:57 by hal-lawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
+
+static void	one_philo_routine(t_thread_args	*c_args)
+{
+	pthread_mutex_lock(&(c_args->forks[0]));
+	custom_print(c_args, "has taken a fork");
+	custom_usleep(c_args->philo->time_to_die, c_args);
+	pthread_mutex_unlock(&(c_args->forks[0]));
+}
+
+static void	sleep_and_think(t_thread_args	*c_args)
+{
+	custom_print(c_args, "sleeping");
+	custom_usleep(c_args->philo->time_to_sleep * 1000, c_args);
+	custom_print(c_args, "thinking");
+	custom_usleep(3000, c_args);
+}
 
 void	*routine(void *args)
 {
@@ -25,16 +41,16 @@ void	*routine(void *args)
 	while (1)
 	{
 		if (c_args->philo->philo_num == 1)
+		{
+			one_philo_routine(c_args);
 			break ;
+		}
 		if (is_end_of_simulation(c_args) == 1)
 			break ;
 		eat(c_args);
 		if (is_end_of_simulation(c_args) == 1)
 			break ;
-		custom_print(c_args, "sleeping");
-		custom_usleep(c_args->philo->time_to_sleep * 1000, c_args);
-		custom_print(c_args, "thinking");
-		custom_usleep(3000, c_args);
+		sleep_and_think(c_args);
 	}
 	free(args);
 	return (NULL);
